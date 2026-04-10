39add4f84b2b944ff2e6dfb39025715054e59497
galbot <galbot@galbotdeMacBook-Air-2.local>
2026-04-10 18:51:50 +0800

    feat: 个人博客修改

--- js/i18n-data.js
+++ js/i18n-data.js
@@ -181,7 +181,7 @@ var I18N_LANG_DATA = {
     publications: {
       title: 'SELECTED PUBLICATIONS',
       subtitle:
-        '*Equal contribution. <span>&#8224;</span>Corresponding author.',
+        '*Equal contribution <span>&#8224;</span>Corresponding author',
     },
     awards: {
       title: 'AWARDS AND HONORS',
