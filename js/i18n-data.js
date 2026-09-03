0e5309d5704e01652df0b45981a16a2fd56f2d0d
galbot <galbot@galbotdeMacBook-Air-2.local>
2026-09-03 18:16:51 +0800

    feat: 博客修改

--- js/i18n-data.js
+++ js/i18n-data.js
@@ -19,7 +19,7 @@ var I18N_LANG_DATA = {
         '<a href="https://www.galbot.com">北京银河通用机器人股份有限公司</a>创始人 CTO',
       subtitle3: '<a href="https://www.bza.edu.cn/">中关村学院</a>研究型导师',
       desc1:
-        '我是<a href="https://english.pku.edu.cn/">北京大学</a><a href="https://cfcs.pku.edu.cn/english">前沿计算机研究中心（CFCS）</a>助理教授、博士生导师，北京大学博雅青年学者，入选国家级海外高层次人才计划。我创立并领导了<a href="https://pku-epic.github.io/">北大具身感知与交互实验室(EPIC Lab)</a>，在通过研发具身泛化技能及具身多模态大模型推进通用机器人的发展。',
+        '我是<a href="https://english.pku.edu.cn/">北京大学</a><a href="https://cfcs.pku.edu.cn/english">前沿计算机研究中心（CFCS）</a>助理教授、博士生导师，北京大学博雅青年学者，入选国家级海外高层次人才计划。我创立并领导了<a href="https://pku-epic.github.io/">北大具身感知与交互实验室(EPIC Lab)</a>，旨在通过研发具身泛化技能及具身多模态大模型推进通用机器人的发展。',
       desc2:
         '同时，我也是<a href="https://www.galbot.com">银河通用（Galbot）</a>的创始人与首席技术官，一家专注于具身智能与通用人形机器人的世界领先独角兽企业；此外，我还兼任<a href="https://www.bza.edu.cn/">北京中关村学院</a>的研究型导师。',
       desc3: '',
